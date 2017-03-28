cbf53fa02255cd25bda036b763ba8a9d3514856f
Steven <s.blake.08@aberdeen.ac.uk>
2017-03-28 18:44:26 +0100

    Updated tests to ensure stub is actually called.

--- spec/client_spec.cr
+++ spec/client_spec.cr
@@ -6,10 +6,12 @@ describe Darksky::Client do
       it "an exception should be raised" do
         expect_raises(Exception, "Received 404 status. Perhaps your API key is incorrect?") do
           WebMock.wrap do
-            WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?lang=en&units=auto").
+            stub = WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?lang=en&units=auto").
               to_return(status: 404)
 
             Darksky.new("apikey").get_forecast("37", "-122")
+
+            stub.calls.should eq(1)
           end
         end
       end
@@ -19,10 +21,12 @@ describe Darksky::Client do
       it "an exception should be raised" do
         expect_raises(Exception, "Received 400 status.") do
           WebMock.wrap do
-            WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?lang=en&units=auto").
+            stub = WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?lang=en&units=auto").
               to_return(status: 400)
 
             Darksky.new("apikey").get_forecast("37", "-122")
+
+            stub.calls.should eq(1)
           end
         end
       end
@@ -32,11 +36,13 @@ describe Darksky::Client do
       lang = Darksky::Language::NL
 
       WebMock.wrap do
-        WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
+        stub = WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
           with(query: {"lang" => lang.text, "units" => "auto"}).
           to_return(body: %({"latitude": 0, "longitude":0, "timezone":""}))
 
         Darksky.new("apikey").get_forecast("37", "-122", lang: lang)
+
+        stub.calls.should eq(1)
       end
     end
 
@@ -44,21 +50,25 @@ describe Darksky::Client do
       units = Darksky::Units::UK2
 
       WebMock.wrap do
-        WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
+        stub = WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
           with(query: {"lang" => "en", "units" => units.text}).
           to_return(body: %({"latitude": 0, "longitude":0, "timezone":""}))
 
         Darksky.new("apikey").get_forecast("37", "-122", units: units)
+
+        stub.calls.should eq(1)
       end
     end
 
     it "should use use extend hourly when specified" do
       WebMock.wrap do
-        WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
+        stub = WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
           with(query: {"lang" => "en", "units" => "auto", "extend" => "hourly"}).
           to_return(body: %({"latitude": 0, "longitude":0, "timezone":""}))
 
         Darksky.new("apikey").get_forecast("37", "-122", extend_hourly: true)
+
+        stub.calls.should eq(1)
       end
     end
 
@@ -66,11 +76,13 @@ describe Darksky::Client do
       exclude = [Darksky::Blocks::Currently, Darksky::Blocks::Daily, Darksky::Blocks::Hourly] 
 
       WebMock.wrap do
-        WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
+        stub = WebMock.stub(:any, "https://api.darksky.net/forecast/apikey/37,-122?").
           with(query: {"lang" => "en", "units" => "auto", "exclude" => exclude.join(",") { |i| i.text } }).
           to_return(body: %({"latitude": 0, "longitude":0, "timezone":""}))
 
         Darksky.new("apikey").get_forecast("37", "-122", exclude: exclude)
+
+        stub.calls.should eq(1)
       end
     end
   end
